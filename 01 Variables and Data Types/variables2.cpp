ccd18810afb6f3a7a04ced556602aa1fab7cbfbb
Ananta <anantajoy007@gmail.com>
2021-06-07 21:36:29 +0600

    data types in cpp

---
+++ 01 Variables and Data Types/variables2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x = 65.5;
+    double y = 66.6;
+    bool b = true;
+
+    cout << x <<endl;
+    cout << y <<endl;
+    cout << b <<endl;
+    cout << sizeof(x) <<endl;
+    cout << sizeof(y) <<endl;
+    cout << sizeof(b) <<endl;
+}
